e9a273b21e198b603b18a0474ec89b49c1efdab2
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-08-16 19:19:50 -0300

    updated project

--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -1,12 +1,17 @@
-import { TouchableOpacity, TouchableOpacityProps, Text } from "react-native";
+import { Pressable, PressableProps, Text } from "react-native";
+// Pressable have more options than that TouchableOpacity
 
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
+  withTiming,
+  Easing,
 } from "react-native-reanimated";
 // useSharedValue - we let's use for save content of animations - this content can change with changing of something state
 // useAnimatedStyle - we let's use for created the animated styles
 // Animated - for become a component in a animated component
+// withSpring - we can use for defined how the value will be altered
+// withTiming - linear animation
 
 import { THEME } from "../../styles/theme";
 import { styles } from "./styles";
@@ -17,7 +22,7 @@ const TYPE_COLORS = {
   MEDIUM: THEME.COLORS.WARNING_LIGHT,
 };
 
-type Props = TouchableOpacityProps & {
+type Props = PressableProps & {
   title: string;
   isChecked?: boolean;
   type?: keyof typeof TYPE_COLORS;
@@ -38,8 +43,16 @@ export function Level({
     return { transform: [{ scale: scale.value }] };
   });
 
+  function onPressIn() {
+    scale.value = withTiming(1.2, { easing: Easing.bounce, duration: 500 });
+  }
+
+  function onPressOut() {
+    scale.value = withTiming(1, { easing: Easing.bounce, duration: 500 });
+  }
+
   return (
-    <TouchableOpacity {...rest}>
+    <Pressable onPressIn={onPressIn} onPressOut={onPressOut} {...rest}>
       <Animated.View
         style={[
           styles.container,
@@ -59,6 +72,6 @@ export function Level({
           {title}
         </Text>
       </Animated.View>
-    </TouchableOpacity>
+    </Pressable>
   );
 }
