150435b40835e46dccb84abbc9485c5ee0ad10ac
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-08-21 23:05:11 -0300

    updated project

--- src/components/Stars/styles.ts
+++ src/components/Stars/styles.ts
@@ -2,7 +2,13 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
     container: {
-        justifyContent: "center",
+        justifyContent: "flex-end",
         alignItems: "center"
+    },
+    canvas: {
+        width: 257, // width and heigh of trophy (look in figma)
+        height: 249,
+        position: "absolute",
+        zIndex: 1
     }
 })
