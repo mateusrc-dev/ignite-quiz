1fa7d3563be50a3661b6c7ad05eb95d0f0cc9d08
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-08-22 14:12:28 -0300

    updated project

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { Alert, Text, View } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
+import { Audio } from "expo-av";
+import * as Haptics from "expo-haptics";
 
 import { QUIZ } from "../../data/quiz";
 import { historyAdd } from "../../storage/quizHistoryStorage";
@@ -61,6 +63,16 @@ export function Quiz() {
   const route = useRoute();
   const { id } = route.params as Params;
 
+  async function playSound(isCorrect: boolean) {
+    const file = isCorrect
+      ? require("../../assets/correct.mp3")
+      : require("../../assets/wrong.mp3");
+    const { sound } = await Audio.Sound.createAsync(file, { shouldPlay: true });
+
+    await sound.setPositionAsync(0);
+    await sound.playAsync();
+  }
+
   function handleSkipConfirm() {
     Alert.alert("Pular", "Deseja realmente pular a questão?", [
       { text: "Sim", onPress: () => handleNextQuestion() },
@@ -97,9 +109,11 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
-      setStatusReply(1);
       setPoints((prevState) => prevState + 1);
+      await playSound(true);
+      setStatusReply(1);
     } else {
+      await playSound(false);
       setStatusReply(2);
       shakeAnimation();
     }
@@ -123,8 +137,9 @@ export function Quiz() {
     return true;
   }
 
-  function shakeAnimation() {
+  async function shakeAnimation() {
     // will be called when the user err a question
+    await Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
     shake.value = withSequence(
       withTiming(3, { duration: 400, easing: Easing.bounce }), // animation run in interface thread of user
       withTiming(0, undefined, (finished) => {
